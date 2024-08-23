58d97aeef73a397946ff526a8046e99b0485c5a0
eduhernandes2f <eduhernandes2f@firemail.com.br>
2024-08-23 10:55:38 -0300

    Update and rename trocavalor.py to 05.trocavalor.py

---
+++ 05.trocavalor.py
@@ -0,0 +1,10 @@
+#Crie um algoritmo que que armazene dois valores distintos (A= 5 e B=10).
+#Depois efetue a troca dos valores de forma que a variável A passe a possuir o valor da variável B e que a variábel B passe a possuir o valor da variável A. 
+#Por fim, apresentar os valores trocados. (A=10 e B = 5)
+a=5
+b=10
+print("Os valores de A e B são respectivamente", a, "e", b)
+c=a
+a=b
+b=c
+print("Agora Ae B valem", a, "e", b)
